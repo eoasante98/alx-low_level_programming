8060f67711a1c6a3f375289b7d471fc051cd25d1
eoasante98 <eo.asante3@gmail.com>
2023-06-16 08:36:33 -0700

    We go low(Variables and Loop Edition)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Code prints single decimal numbers from 0.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		printf("%d");
+	}
+	purchar('\n');
+	return (0);
+}
