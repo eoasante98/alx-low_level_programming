a7fa32308441ce0a30a43cae15df22eeb8842341
eoasante98 <eo.asante3@gmail.com>
2023-06-16 08:52:05 -0700

    We go low(Variables and Loop Edition)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints something. Read the code yourself.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
