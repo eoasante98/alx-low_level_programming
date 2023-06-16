863eb51d6f0a417628ba833d4e82e56c07ee6166
eoasante98 <eo.asante3@gmail.com>
2023-06-15 17:19:35 -0700

    We go low

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - entry point
+ *
+ * int - return type
+ *
+ * Description : prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
