7df290444d3a52b2de29d7e7244b2644429a746b
eoasante98 <eo.asante3@gmail.com>
2023-06-15 18:08:59 -0700

    We go low

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a double: %lu byte(s)\n", sizeof(double));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a long: %lu byte(s)\n", sizeof(long));
-	printf("Size of a short: %lu byte(s)\n"' sizeof(short));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a double: %d byte(s)\n", sizeof(double));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long: %d byte(s)\n", sizeof(long));
+	printf("Size of a short: %d byte(s)\n"' sizeof(short));
 	return (0);
 }
