ddd6f7964e7f0d631f05bc9895e7926aa62312af
eoasante98 <eo.asante3@gmail.com>
2023-06-16 07:14:18 -0700

    We go low(Variables and Loop Edition)

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: The code prints alphabets in lowercase.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
