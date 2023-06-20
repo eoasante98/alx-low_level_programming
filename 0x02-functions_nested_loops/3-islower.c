34dfdc01dd286c3bc6feb9b501a94221a6a76496
eoasante98 <eo.asante3@gmail.com>
2023-06-20 16:54:24 -0700

    Down into the nested loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,17 +4,19 @@
  *
  * Description: shows 1 for lowercase, 0 for others
  *
+ * @c: ANSCII code
+ *
  * Return: 1 for lowercase, 0 otherwise
  */
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
