2a88c6e6999bce3c11d5ae53e136ee816665d93d
eoasante98 <eo.asante3@gmail.com>
2023-06-21 09:14:58 -0700

    Bug-a-roo

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,12 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);/*an increment should follow*/
-	}
+	/**
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
