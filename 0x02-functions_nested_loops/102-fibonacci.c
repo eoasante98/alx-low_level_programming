4ae6e966f453597839359fcb981df29f3730e766
eoasante98 <eo.asante3@gmail.com>
2023-06-20 18:53:26 -0700

    Further down into the nested loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
-	printf("%ld, %ld". n1, n2);
+	printf("%ld, %ld", n1, n2);
 	for (a = 0; a < 48; a++)
 	{
 		fn = n1 + n2;
@@ -19,6 +19,6 @@ int main(void)
 		n1 = n2;
 		n2 = fn;
 	}
-	print("\n");
+	printf("\n");
 	return (0);
 }
