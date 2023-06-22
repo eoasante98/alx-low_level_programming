2772d2a732afdbeaa0e3d485ac1dc3476da2eac3
eoasante98 <eo.asante3@gmail.com>
2023-06-22 11:02:09 -0700

    ei, hmm

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,17 +2,21 @@
 /**
  * print_line - as said
  *
+ * @n: input value taken
+ *
  * Return: void
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar(10);
 	}
 	else
 	{
-		for (int i = 0; i <= n; i++)
-			_putchar("_");
+		for (i = 0; i <= n; i++)
+			_putchar(95);
 	}
 }
