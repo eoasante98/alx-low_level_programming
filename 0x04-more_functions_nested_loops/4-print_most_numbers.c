7ef5415982ce6b7c3ffd1abcca54ceb4f3ae9aec
eoasante98 <eo.asante3@gmail.com>
2023-06-22 08:08:24 -0700

    Headstrong

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 47; i < 58; i++)
+	for (i > 47; i < 58; i++)
 	{
-		if (i != 49 || i != 51)
+		if (i != 50 || i != 52)
 			_putchar(i);
 	}
 	_putchar(10);
