c37afba9056f72018846d1bf791f88f2ec027975
eoasante98 <eo.asante3@gmail.com>
2023-06-22 07:34:06 -0700

    Headstrong

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - as said
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i > 47; i < 58; i++)
+	{
+		if (i != '2' || i != '4')
+			_putchar(i);
+	}
+	_putchar(10);
+}
