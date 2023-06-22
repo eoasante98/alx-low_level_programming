10860f1b328b8b3eac4528373d86d24579bee305
eoasante98 <eo.asante3@gmail.com>
2023-06-22 07:21:35 -0700

    Grit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * print_numbers - prints the number, from 0 to 9, followed by a new line
+ * print_numbers - entry point
  *
- * Return: 0 to 9
+ * Return: void
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar(10);
 }
