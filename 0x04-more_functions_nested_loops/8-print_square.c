e105b214ff924e8a2ba49bc62d785709cff38307
eoasante98 <eo.asante3@gmail.com>
2023-06-22 18:21:01 -0700

    Ham

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,20 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < (size - 1); j++)
-		{
-			_putchar(35);
-		}
-		_putchar(35);
 		_putchar(10);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - 1); j++)
+			{
+				_putchar(35);
+			}
+			_putchar(35);
+			_putchar(10);
+		}
 	}
 }
