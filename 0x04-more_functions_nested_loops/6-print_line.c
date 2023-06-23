8dbc7c94e62e68c3924eec20ff4ca0ee7007e91b
eoasante98 <eo.asante3@gmail.com>
2023-06-23 01:03:46 -0700

    Rise and shine, your hearts is mine

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -18,12 +18,8 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar(10);
+			_putchar(95);
 		}
+		_putchar(10);
 	}
 }
