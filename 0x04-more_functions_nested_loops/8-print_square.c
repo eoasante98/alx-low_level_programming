b4ce7fe727578edc8b52309bdb8fcb3d246d50a9
eoasante98 <eo.asante3@gmail.com>
2023-06-22 18:14:43 -0700

    Ham

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints square, followed by a line
+ *
+ * @size: input's container
+ *
+ * Return: size
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			_putchar(35);
+		}
+		_putchar(35);
+		_putchar(10);
+	}
+}
