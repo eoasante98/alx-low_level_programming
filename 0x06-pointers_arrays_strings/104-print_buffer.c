53b343bf47a76f4ddd47f6196f1eb183de33b55e
eoasante98 <eo.asante3@gmail.com>
2023-06-29 19:14:23 -0700

    huuuh

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -22,7 +22,7 @@ void print_buffer(char *b, int size)
 				if (k % 2 == 0)
 					printf(" ");
 				if (k < size)
-					printf("%.2x", *(b + k) & 0xFF);
+					printf("%.2x", *(b + k));
 				else
 					printf(" ");
 			}
