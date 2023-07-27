2a0b881d41910c053c9f817ed313c51f47f94f81
eoasante98 <eo.asante3@gmail.com>
2023-07-27 16:10:10 -0700

    0-print... updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	{
 		elements++;
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[%d] (nil)\n", 0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
