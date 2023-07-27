92b99f29db904f9b08b12c9724767456b1987285
eoasante98 <eo.asante3@gmail.com>
2023-07-27 15:35:00 -0700

    A list, linked! Again

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * list_len - returns the number of elements in alist
+ * @h: singly linked list
+ * Return: number of elements in question
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		 h = h->next;
+		 elements++;
+	}
+	return (elements);
+}
