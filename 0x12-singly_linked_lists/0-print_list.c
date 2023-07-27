b19ce8a6a9b652f34dee4cffd517d1a60de6808c
eoasante98 <eo.asante3@gmail.com>
2023-07-27 16:07:52 -0700

    0-print... updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t print_list(const list_t *h)
 	elements = 0;
 	while (h != NULL)
 	{
+		elements++;
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		elements++;
 	}
 	return (elements);
 }
