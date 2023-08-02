1d6f5ac6d23bbb0a9f2b5f280344c2b2495e58f5
eoasante98 <eo.asante3@gmail.com>
2023-08-01 21:30:58 -0700

    adding some...

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,12 +6,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t nnodes = 0;
+	size_t nnodes = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                nnodes++;
-        }
-        return (nnodes);
+       while (h != NULL)
+       {
+	       h = h->next;
+	       nnodes++;
+       }
+       return (nnodes);
 }
