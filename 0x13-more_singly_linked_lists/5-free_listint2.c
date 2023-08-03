fc3cf338fc031dade352c3ba16acd73fb10a9562
eoasante98 <eo.asante3@gmail.com>
2023-08-02 17:15:27 -0700

    adding others...

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_;istint2 - free a linked list
+ * free_listint2 - free a linked list
  * @head: ...
  * Return: no return
  */
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,5 +15,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 	}
 
-	rerurn (head);
+	return (head);
 }
