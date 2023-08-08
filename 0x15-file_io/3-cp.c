13cd274fffd04dd697ed97721ef5024e10fd0f16
eoasante98 <eo.asante3@gmail.com>
2023-08-07 17:12:40 -0700

    updating 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,7 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_frm, file_to, err_close;
+	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
