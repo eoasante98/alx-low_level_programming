f5b3714cf6c08c66e874680f121207039530923d
eoasante98 <eo.asante3@gmail.com>
2023-08-11 08:01:57 -0700

    test...

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <elf.h>
+#include <stdio.h>
 
+int main(int argc, char *argv[]);
+int elf_header_main(int argc, char *argv[]);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
