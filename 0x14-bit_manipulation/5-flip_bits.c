b8cf1a80ed9d0bd8f9d0e82d41997e214b517834
eoasante98 <eo.asante3@gmail.com>
2023-08-23 06:43:34 -0700

    updating 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@ unsigned int flip_bits(unsigned long int n, unsigned int long m)
 {
 	unsigned int nbits;
 
-	for (nbits = 0; n || m; n >>= 1, m >>=1)
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			nbits++;
