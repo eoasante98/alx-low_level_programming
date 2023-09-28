b0f539bb0b41aae01f8a0cb65f1a788af615fda1
eoasante98 <eo.asante3@gmail.com>
2023-09-28 14:37:24 -0700

    updating 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int trun;
 
-	if (index >= sizeof(unsigned long int) *8 )
+	if (index >= sizeof(unsigned long int) * 8 )
 		return (-1);
 
 	trun = 1UL << index;
